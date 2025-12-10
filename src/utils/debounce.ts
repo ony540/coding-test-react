f30751f219c07068ed0d07158664810e28b8422b
nayeong.oh <onyy540@gmail.com>
2025-12-10 11:33:48 +0900

    과제 2: Debounce 함수 구현

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -17,10 +17,13 @@
 export function debounce<T extends (...args: any[]) => any>(func: T, delay: number): (...args: Parameters<T>) => void {
   // 여기에 debounce 로직을 구현하세요.
   // Hint: 클로저를 사용하여 타이머 ID를 관리해야 합니다.
+  let timer: number | undefined;
   return function(...args: Parameters<T>) {
     // @ts-ignore
     const context = this;
-    // ...
-    func.apply(context, args);
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
   };
 }
